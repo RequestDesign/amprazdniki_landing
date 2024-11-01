cf1af448b03154b7147ae456a501317a40f7d812
zatzoid <zatzoid@gmail.com>
2024-11-01 18:16:17 +0300

    v1

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,18 +1,15 @@
 import $ from "jquery";
 
 $(function(){
-	$('.header-burger').click(function(){
-		$('body').toggleClass('lock');
-        $(this).parents('.header-container').toggleClass('active')
-	});
+
 
 	$(window).scroll(function () {
 		var scrollPosition = $(window).scrollTop()
 
 		if(scrollPosition > 0) {
-			$('.header').addClass('on-top')
+			$('.header').fadeOut()
 		} else {
-			$('.header').removeClass('on-top')
+			$('.header').fadeIn()
 		}
 	})
 
--- src/js/dev/zatz.js
+++ src/js/dev/zatz.js
@@ -14,7 +14,7 @@ $(function () {
     initWow()
     initFancybox()
     initSwipers()
- 
+    imagesHover()
 })
 
 function initForms() {
@@ -46,80 +46,30 @@ function dropDowns() {
 
 }
 function initSwipers() {
-    const heading = document.querySelector('.heading')
-    if (heading) {
-        const target = new Swiper(heading.querySelector('.swiper'), {
-            modules: [Pagination, EffectFade, Autoplay],
-            loop: false,
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-            effect: 'fade',
-            autoplay: { delay: 3000 },
-            pagination: {
-                el: heading.querySelector('.swiper-pagination'),
-                clickable: true
-            },
-        })
-        const counter = heading.querySelector('.swiper-counter')
-        if (counter) {
-            counter.querySelector('.swiper-counter-all').textContent = `0${target.slides.length}`
-            const counterCurr = counter.querySelector('.swiper-counter-current');
-            target.on('slideChange', function (e) {
-                counterCurr.textContent = `0${target.activeIndex + 1}`
-            });
-        }
-
-
-    }
-
-    const prices = document.querySelector('.prices')
-    if (prices) {
-        new Swiper(prices.querySelector('.swiper'), {
-            loop: false,
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-            spaceBetween: rem(4),
-            autoHeight: true,
-            breakpoints: {
-                768: {
-                    slidesPerView: 3
-                }
-            }
-
-        })
-
-    }
-
+  
     const sliders = document.querySelectorAll('.slider')
     if (sliders) {
         sliders.forEach((slider) => {
-            const counter = slider.querySelector('.swiper-counter')
-            const counterCurr = counter.querySelector('.swiper-counter-current');
-            const target = new Swiper(slider.querySelector('.swiper'), {
-                modules: [Pagination, EffectCreative, Navigation],
-                loop: false,
-                speed: 1000,
-                initialSlide: 1,
-                slideToClickedSlide: true,
-                effect: window.innerWidth < 769 ? "slide" : "creative",
-                slidesPerView: "auto",
-                spaceBetween: rem(4),
+            new Swiper(slider.querySelector('.swiper'), {
+                modules: [Navigation, Pagination, EffectCreative],
+                loop: true,
+                effect: window.innerWidth < 769 ? "creative" : "slide",
+                slidesPerView: 1.5,
+                creativeEffect: {
+                    prev: {
 
+                        translate: ["-125%", 30, -300],
+                    },
+                    next: {
+
+                        translate: ["50%", 30, -400],
+                    },
+                    limitProgress: 2
+                },
                 breakpoints: {
                     769: {
-                        slidesPerView: 3,
-                        creativeEffect: {
-                            prev: {
-                                shadow: true,
-                                translate: ["-125%", 30, -300],
-                            },
-                            next: {
-                                shadow: true,
-                                translate: ["125%", 30, -300],
-                            },
-                        },
-                        spaceBetween: 0,
-                        centeredSlides: true,
+                        spaceBetween: rem(2),
+                        slidesPerView: 'auto',
                     },
                 },
                 pagination: {
@@ -127,68 +77,26 @@ function initSwipers() {
                     clickable: true
                 },
                 navigation: {
-                    nextEl: slider.querySelector('.slider__aside-btn')
+                    prevEl: slider.querySelector('.swiper-btn-prev'),
+                    nextEl: slider.querySelector('.swiper-btn-next')
                 },
                 on: {
-                    init: function (swiper) {
-                        counter.querySelector('.swiper-counter-all').textContent = `0${swiper.slides.length}`
-                    },
-                    slideChange: function (swiper) {
-                        counterCurr.textContent = `0${swiper.activeIndex + 1}`
-                    },
+                    slideChange: (swiper) => {
+                        swiper.slides.forEach((slide) => {
+                            slide.classList.remove('_hover');
+                        });
+                        const nextSlide = swiper.el.querySelector('.swiper-slide-next');
+                        if (nextSlide) {
+                            nextSlide.classList.add('_hover');
+                        }
+
+                    }
                 }
 
-            })
-
-
-            window.addEventListener("load", () => {
-                if (window.innerWidth < 769) {
-                    target.params.effect = "slide";
-                } else {
-                    target.params.effect = "creative";
-                }
             });
-
         })
 
 
-    }
-
-    const reviews = document.querySelector('.reviews')
-    if (reviews) {
-        const counter = reviews.querySelector('.swiper-counter')
-        const counterCurr = counter.querySelector('.swiper-counter-current');
-        new Swiper(reviews.querySelector('.swiper'), {
-            modules: [Navigation, Pagination],
-            /*     speed: 2000, */
-            /* slidesPerView: 5, */
-            loop: true,
-            /*  slideToClickedSlide: true, */
-            // mousewheel: true,
-            spaceBetween: rem(2.5),
-            breakpoints: {
-                769: {
-                    slidesPerView: 'auto',
-                },
-            },
-            pagination: {
-                el: reviews.querySelector('.swiper-pagination'),
-                clickable: true
-            },
-            navigation: {
-                prevEl: reviews.querySelector('.swiper-btn-prev'),
-                nextEl: reviews.querySelector('.swiper-btn-next')
-            },
-            on: {
-                init: function (swiper) {
-                    counter.querySelector('.swiper-counter-all').textContent = swiper.slides.length.toString().padStart(2, '0')
-                },
-                slideChange: function (swiper) {
-                    counterCurr.textContent = (swiper.realIndex + 1).toString().padStart(2, '0')
-                },
-
-            }
-        });
     }
 
 }
@@ -255,5 +163,28 @@ function modalsHandler() {
         }
         /* html.removeClass('lock') */
     })
+}
+
+function imagesHover() {
+    const containers = document.querySelectorAll('.slider._images');
+    if (!containers) return
+
+    containers.forEach((c) => {
+        const slides = c.querySelectorAll('.swiper-slide'),
+            hover = '_hover'
+
+
+        slides.forEach((s) => {
+            if (s.classList.contains('swiper-slide-next')) {
+                s.classList.add('_hover')
+            }
+            s.addEventListener('mouseover', (ev) => {
+                c.querySelector('.swiper-slide._hover').classList.remove(hover)
+                ev.currentTarget.classList.add(hover)
+            })
+        })
+
+    })
+
 }
 
